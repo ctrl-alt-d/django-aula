930f90c4870a0a54767ca8a105ea9064d4e74463
Joan Rodríguez <jrodr236@gmail.com>
2013-07-27 13:30:45 +0200

    Nou script per a la importacio inicial dels fixtures

---
+++ fixtures.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+for i in extKronowin incidencies presencia assignatures horaris seguimentTutorial missatgeria usuaris
+do
+    python manage.py loaddata aula/apps/$i/fixtures/dades.json
+done
