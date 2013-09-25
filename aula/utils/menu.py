63af089bdb13bc85af0de2462bdc3c9c3549e6ed
dani <dani@dani-ghap-bi.(none)>
2013-09-25 16:13:35 +0200

    Professors, passar llista

--- aula/utils/menu.py
+++ aula/utils/menu.py
@@ -160,12 +160,9 @@ def calcula_menu( user , path ):
                             ('Tutors individualitzat', 'professorat__tutors__tutors_individualitzats', di, None),
                          ), 
                       ),
-                      ("Professors", 'professorat__professors__list', di, None, None, ),
+                      ("Professors", 'professorat__professors__list', di, None, None ),
+        
                       ),
-                   
-                   
-                   professorat__professors__list
-                   
                    )
                ),
 
