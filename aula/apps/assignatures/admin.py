9290bcd9d16b2252fe85f0e0bbf1e7a402a893d6
jrodr236 <jrodr236@gmail.com>
2014-09-05 23:42:12 +0200

    Administració assignatures

--- aula/apps/assignatures/admin.py
+++ aula/apps/assignatures/admin.py
@@ -1,6 +1,10 @@
 from aula.apps.assignatures.models import TipusDAssignatura, Assignatura
 from django.contrib import admin
 
-admin.site.register(TipusDAssignatura)
-admin.site.register(Assignatura)
+class AssignaturaAdmin(admin.ModelAdmin):
+    model = Assignatura
+    list_display= ['codi_assignatura', 'nom_assignatura', 'curs', 'tipus_assignatura']
+    list_filter = ['curs', 'tipus_assignatura']
 
+admin.site.register(TipusDAssignatura)
+admin.site.register(Assignatura, AssignaturaAdmin)
