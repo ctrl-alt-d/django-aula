e6f39d2610589354d25c7b7a8da567c541e7c0a4
dani <dani@dani-ghap-bi.(none)>
2015-11-25 15:57:37 +0100

    notifica families. treure autocommit

--- aula/apps/relacioFamilies/notifica.py
+++ aula/apps/relacioFamilies/notifica.py
@@ -21,102 +21,102 @@ def notifica():
     urlDjangoAula = settings.URL_DJANGO_AULA
     textTutorial = settings.CUSTOM_PORTAL_FAMILIES_TUTORIAL
     
-    with transaction.autocommit():
+    #with transaction.autocommit():  #deprecated on 1.8. Now is the default behaviuor.
         
-        #actualitzo notificacions sortides:
-        notifica_sortides()
+    #actualitzo notificacions sortides:
+    notifica_sortides()
 
-        #Notificacions        
-        ara = datetime.now()
-        
-        fa_2_setmanes = ara - timedelta(  days = 14 )
-        presencies_notificar = EstatControlAssistencia.objects.filter( codi_estat__in = ['F','R','J']  )
-        q_no_es_baixa = Q(data_baixa__gte = ara ) | Q(data_baixa__isnull = True )
-        q_no_informat_adreca = Q( correu_relacio_familia_pare = '' ) & Q( correu_relacio_familia_mare = '' )
-        
-        llista_alumnes = Alumne.objects.filter(q_no_es_baixa).exclude( q_no_informat_adreca ).values_list('pk', flat=True)
-        
-        for alumne_id in llista_alumnes:
-            try:
-                alumne = Alumne.objects.get( pk = alumne_id )
-                fa_n_dies = ara - timedelta(  days = alumne.periodicitat_faltes )
-                noves_sortides = NotificaSortida.objects.filter( alumne = alumne, relacio_familia_notificada__isnull = True  )
-                noves_incidencies = alumne.incidencia_set.filter( relacio_familia_notificada__isnull = True  )
-                noves_expulsions = alumne.expulsio_set.exclude( estat = 'ES').filter(    relacio_familia_notificada__isnull = True  )
-                noves_sancions = alumne.sancio_set.filter( impres=True, relacio_familia_notificada__isnull = True  )
-                noves_faltes_assistencia = ControlAssistencia.objects.filter( alumne = alumne, 
-                                                                              impartir__dia_impartir__gte = fa_2_setmanes,
-                                                                              relacio_familia_notificada__isnull = True,
-                                                                              estat__pk__in = presencies_notificar )
-                hiHaNovetatsPresencia = False
-                hiHaNovetats = False
-                #comprovo si hi ha novetats de presencia i incidències
-                fa_dies_que_no_notifiquem = alumne.relacio_familia_darrera_notificacio is None or \
-                                            alumne.relacio_familia_darrera_notificacio < fa_n_dies
-                hiHaNovetatsPresencia =  alumne.periodicitat_faltes > 0 and \
-                                         fa_dies_que_no_notifiquem and \
-                                         noves_faltes_assistencia.exists()
-                hiHaNovetats =  (
-                                 hiHaNovetatsPresencia or 
-                                 noves_sortides.exists() or
-                                    ( alumne.periodicitat_incidencies and
-                                    ( noves_incidencies.exists() or noves_expulsions.exists() or noves_sancions.exists() )
-                                    )
-                                 )                  
-                #print u'Avaluant a {0}'.format( alumne )
-                enviatOK = False
-                if hiHaNovetats:
-                    #enviar correu i marcar novetats com a notificades:
-                    missatge = [ 
-                             u"Portal d'informació a les famílies de " + settings.NOM_CENTRE,
-                             u"",
-                             u"Teniu novetats de {0} al portal de relació famílies {1}".format(alumne.nom, urlDjangoAula),
-                             u"",
-                             u"Recordeu que el vostre nom d'usuari és: {0}".format( alumne.get_user_associat().username ),
-                             u"",
-                             u"Esperem que amb aquesta aplicació us poguem ajudar a fer un seguiment més exahustiu del treball dels vostres fills al centre.",
-                             u"",
-                             u"Cordialment",
-                             u"",
-                             settings.NOM_CENTRE,
-                             u"",
-                             u"",
-                             u"{0}".format( textTutorial ),
-                            ]
-                    
-                    try:                        
-                        fromuser = settings.EMAIL_HOST_USER
-                        if settings.DEBUG:
-                            print u'Enviant missatge a {0}'.format( alumne )
-                        send_mail(u'Novetats al portal de relació famílies', 
-                                      u'\n'.join( missatge ), 
-                                      fromuser,
-                                      alumne.get_correus_relacio_familia(), 
-                                      fail_silently=False)
-                        enviatOK = True
-                    except:
-                        #cal enviar msg a tutor que no s'ha pogut enviar correu a un seu alumne.
-                        enviatOK = False
+    #Notificacions        
+    ara = datetime.now()
+    
+    fa_2_setmanes = ara - timedelta(  days = 14 )
+    presencies_notificar = EstatControlAssistencia.objects.filter( codi_estat__in = ['F','R','J']  )
+    q_no_es_baixa = Q(data_baixa__gte = ara ) | Q(data_baixa__isnull = True )
+    q_no_informat_adreca = Q( correu_relacio_familia_pare = '' ) & Q( correu_relacio_familia_mare = '' )
+    
+    llista_alumnes = Alumne.objects.filter(q_no_es_baixa).exclude( q_no_informat_adreca ).values_list('pk', flat=True)
+    
+    for alumne_id in llista_alumnes:
+        try:
+            alumne = Alumne.objects.get( pk = alumne_id )
+            fa_n_dies = ara - timedelta(  days = alumne.periodicitat_faltes )
+            noves_sortides = NotificaSortida.objects.filter( alumne = alumne, relacio_familia_notificada__isnull = True  )
+            noves_incidencies = alumne.incidencia_set.filter( relacio_familia_notificada__isnull = True  )
+            noves_expulsions = alumne.expulsio_set.exclude( estat = 'ES').filter(    relacio_familia_notificada__isnull = True  )
+            noves_sancions = alumne.sancio_set.filter( impres=True, relacio_familia_notificada__isnull = True  )
+            noves_faltes_assistencia = ControlAssistencia.objects.filter( alumne = alumne, 
+                                                                          impartir__dia_impartir__gte = fa_2_setmanes,
+                                                                          relacio_familia_notificada__isnull = True,
+                                                                          estat__pk__in = presencies_notificar )
+            hiHaNovetatsPresencia = False
+            hiHaNovetats = False
+            #comprovo si hi ha novetats de presencia i incidències
+            fa_dies_que_no_notifiquem = alumne.relacio_familia_darrera_notificacio is None or \
+                                        alumne.relacio_familia_darrera_notificacio < fa_n_dies
+            hiHaNovetatsPresencia =  alumne.periodicitat_faltes > 0 and \
+                                     fa_dies_que_no_notifiquem and \
+                                     noves_faltes_assistencia.exists()
+            hiHaNovetats =  (
+                             hiHaNovetatsPresencia or 
+                             noves_sortides.exists() or
+                                ( alumne.periodicitat_incidencies and
+                                ( noves_incidencies.exists() or noves_expulsions.exists() or noves_sancions.exists() )
+                                )
+                             )                  
+            #print u'Avaluant a {0}'.format( alumne )
+            enviatOK = False
+            if hiHaNovetats:
+                #enviar correu i marcar novetats com a notificades:
+                missatge = [ 
+                         u"Portal d'informació a les famílies de " + settings.NOM_CENTRE,
+                         u"",
+                         u"Teniu novetats de {0} al portal de relació famílies {1}".format(alumne.nom, urlDjangoAula),
+                         u"",
+                         u"Recordeu que el vostre nom d'usuari és: {0}".format( alumne.get_user_associat().username ),
+                         u"",
+                         u"Esperem que amb aquesta aplicació us poguem ajudar a fer un seguiment més exahustiu del treball dels vostres fills al centre.",
+                         u"",
+                         u"Cordialment",
+                         u"",
+                         settings.NOM_CENTRE,
+                         u"",
+                         u"",
+                         u"{0}".format( textTutorial ),
+                        ]
+                
+                try:                        
+                    fromuser = settings.EMAIL_HOST_USER
+                    if settings.DEBUG:
+                        print u'Enviant missatge a {0}'.format( alumne )
+                    send_mail(u'Novetats al portal de relació famílies', 
+                                  u'\n'.join( missatge ), 
+                                  fromuser,
+                                  alumne.get_correus_relacio_familia(), 
+                                  fail_silently=False)
+                    enviatOK = True
+                except:
+                    #cal enviar msg a tutor que no s'ha pogut enviar correu a un seu alumne.
+                    enviatOK = False
 
-                if enviatOK:                    
-                    noves_sortides.update( relacio_familia_notificada = ara )
-                    noves_incidencies.update( relacio_familia_notificada = ara )
-                    noves_expulsions.update( relacio_familia_notificada = ara )
-                    noves_sancions.update( relacio_familia_notificada = ara )
-                    noves_faltes_assistencia.update( relacio_familia_notificada = ara )
-                    #if hiHaNovetatsPresencia:
-                    alumne.relacio_familia_darrera_notificacio = ara
-                    alumne.save()
+            if enviatOK:                    
+                noves_sortides.update( relacio_familia_notificada = ara )
+                noves_incidencies.update( relacio_familia_notificada = ara )
+                noves_expulsions.update( relacio_familia_notificada = ara )
+                noves_sancions.update( relacio_familia_notificada = ara )
+                noves_faltes_assistencia.update( relacio_familia_notificada = ara )
+                #if hiHaNovetatsPresencia:
+                alumne.relacio_familia_darrera_notificacio = ara
+                alumne.save()
 
-                    #LOGGING
-                    Accio.objects.create( 
-                            tipus = 'NF',
-                            usuari = alumne.get_user_associat(),
-                            l4 = False,
-                            impersonated_from = None,
-                            text = u"""Notifica Relació Famílies a {0}.""".format( alumne )
-                        )   
-                                    
-            except ObjectDoesNotExist:
-                pass
+                #LOGGING
+                Accio.objects.create( 
+                        tipus = 'NF',
+                        usuari = alumne.get_user_associat(),
+                        l4 = False,
+                        impersonated_from = None,
+                        text = u"""Notifica Relació Famílies a {0}.""".format( alumne )
+                    )   
+                                
+        except ObjectDoesNotExist:
+            pass
 
