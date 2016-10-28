2684ffc9dcdea6179baa912e6d92d051488e962e
ctrl-alt-d <ctrl.alt.d@gmail.com>
2016-10-28 10:15:41 +0200

    Update settings.py

--- aula/settings.py
+++ aula/settings.py
@@ -11,8 +11,16 @@ CUSTOM_DIES_PRESCRIU_INCIDENCIA = 30
 CUSTOM_DIES_PRESCRIU_EXPULSIO = 60
 CUSTOM_MODUL_SORTIDES_ACTIU = False
 CUSTOM_PERMET_COPIAR_DES_DUNA_ALTRE_HORA = False
+
+#Si True, permet que els tutors tinguin accés als informes de seguiment de faltes i incidències.
 CUSTOM_TUTORS_INFORME = False
+
+#URL on trobar el tutorial del portal famílies
 CUSTOM_PORTAL_FAMILIES_TUTORIAL = u""
+
+#Número de faltes no justificades per tal de generar carta
+#Els tipus de carta els trobareu a:
+#      aula/apps/tutoria/business_rules/cartaaabsentisme.py
 CUSTOM_FALTES_ABSENCIA_PER_CARTA = 15
 CUSTOM_FALTES_ABSENCIA_PER_TIPUS_CARTA = { 'tipus1': 20 }
 
