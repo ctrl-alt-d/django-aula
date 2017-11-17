8c6fde37b988503787505f19678d97baa17adce9
ctrl-alt-d <ctrl.alt.d@gmail.com>
2017-11-17 18:45:40 +0100

    Cerca professor L4

--- aula/apps/usuaris/forms.py
+++ aula/apps/usuaris/forms.py
@@ -25,7 +25,12 @@ class triaUsuariForm(forms.Form):
     professor = forms.ModelChoiceField( 
                         queryset= Professor.objects.all(),
                         required= True,
-                        help_text=u'Tria en quin usuari et vols convertir.',   
+                        help_text=u'Tria en quin usuari et vols convertir.',
+                        widget=ModelSelect2Widget(
+                            queryset=Professor.objects.all(),
+                            search_fields=['last_name__icontains', 'first_name__icontains'],
+                            attrs={'style': "'width': '100%'"}
+                        ),
                     )
 
 
