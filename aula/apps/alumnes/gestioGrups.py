13bad0c7796e9950024a762651f3ad512544629d
antonio <antonio@pc9>
2019-10-20 17:13:29 +0200

    Corregit selecció grups.
    Corregit error en un union de querysets

--- aula/apps/alumnes/gestioGrups.py
+++ aula/apps/alumnes/gestioGrups.py
@@ -22,20 +22,25 @@ def grupsAgrupament(grup):
 
     '''
     
-    q_Agrup=Agrupament.objects.filter(grup_horari=grup).values('grup_alumnes')
+    q_Agrup=Agrupament.objects.filter(grup_horari=grup).values('grup_alumnes').distinct()
     if not(q_Agrup.exists()):
         # No hi ha agrupament
-        return Grup.objects.filter(pk = grup.pk)
+        return Grup.objects.filter(pk = grup.pk).filter(alumne__isnull = False).distinct()
     if q_Agrup.count()==1 and q_Agrup.first()['grup_alumnes']==grup.id:
         # Agrupament és el mateix grup
-        return Grup.objects.filter(pk = grup.pk)
+        return Grup.objects.filter(pk = grup.pk).filter(alumne__isnull = False).distinct()
     # Agrupament de dos o més elements
     query=Grup.objects.none()
     for g in q_Agrup:
-        ng=Grup.objects.get(pk = g['grup_alumnes'])
-        # No es tenen en compte els elements que siguin el mateix grup 
-        if (ng==grup): continue
-        query=query.union(grupsAgrupament(ng))
+        ng=Grup.objects.filter(pk = g['grup_alumnes']).filter(alumne__isnull = False).distinct()
+        if not ng:
+            continue
+        if (ng.first()==grup):
+            # Si és el mateix grup, s'afegeix sense fer més recerca
+            query=query.union(ng)
+        else:
+            # Es continua la recerca
+            query=query.union(grupsAgrupament(ng.first()))
     return query
 
 #  amorilla@xtec.cat
@@ -57,9 +62,11 @@ def grupsPotencials(horari):
     elif codi_ambit[0] == 'A':
         q_grups=grupsAgrupament(horari.grup)
         if codi_ambit=='AN':
-            q_grupsN=Grup.objects.filter( curs__nivell__nom_nivell__in = q_grups.values('curs__nivell__nom_nivell')).distinct()
-            q_grups=q_grups.union(q_grupsN)
-        grups_potencials= q_grups.filter(alumne__isnull = False)
+            llista=q_grups.values('curs__nivell__nom_nivell')
+            valors=[]
+            for i in llista: valors.append(i.get('curs__nivell__nom_nivell'))
+            q_grups=Grup.objects.filter( curs__nivell__nom_nivell__in = valors)
+        grups_potencials= q_grups.filter(alumne__isnull = False).distinct()
     elif codi_ambit == 'G':
         if horari.grup:
             grups_potencials=Grup.objects.filter( pk = horari.grup.pk  )
