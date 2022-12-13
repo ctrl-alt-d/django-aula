cf87a618b0f6431614df7ef89a4060585caad7fb
user <juaky73@gmail.com>
2022-12-13 17:42:25 +0100

    api mbl bk

--- aula/mblapp/views.py
+++ aula/mblapp/views.py
@@ -4,7 +4,7 @@ from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated,AllowAny
 from rest_framework.response import Response
 from aula.mblapp.security_rest import EsUsuariVinculatAEstudiant
-
+import uuid
 
 @api_view(['GET'])
 @permission_classes((AllowAny, ))
@@ -30,3 +30,8 @@ def hello_api_login_app(request, format=None):
         'status': 'here we are login group app'
     }
     return Response(content)
+
+def genera_clau( ):
+    clau = str( uuid.uuid4() )
+    #google url = https://chart.googleapis.com/chart?cht=qr&chl=701fe92c-4676-4863-9dd5-6c64d019efc7&chs=300x300
+    return clau
