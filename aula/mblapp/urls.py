32cfccc3eb3e481c155b3b1c440324563032181c
user <juaky73@gmail.com>
2022-12-14 23:08:48 +0100

    backup2

--- aula/mblapp/urls.py
+++ aula/mblapp/urls.py
@@ -10,5 +10,6 @@ urlpatterns = [
         name='mblapp__hello__apilogin'),
     re_path(r'^hello_api_login_app/$', mblapp_views.hello_api_login_app,
         name='mblapp__hello__apiloginapp'),
-
+    re_path(r'^capture_token_api/$', mblapp_views.capture_token_api,
+        name='mblapp__api__capture_token_api'),
 ]
