c0817fcbe21ea983d1e7815503182159b36418f0
user <juaky73@gmail.com>
2023-03-26 23:38:36 +0200

    authenticacio JWT

--- aula/settings_dir/common.py
+++ aula/settings_dir/common.py
@@ -227,11 +227,14 @@ INSTALLED_APPS_AULA = [
 AUTO_RENDER_SELECT2_STATICS=False
 #---
 
-#rest framework
+#rest framework & JWT
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': (
         'rest_framework.permissions.IsAuthenticated',
-    )
+    ),
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
+    ),
 }
 #---
 
