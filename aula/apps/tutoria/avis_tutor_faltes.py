c3c929e8134796c1224778be10b52276678d9650
user <juaky73@gmail.com>
2024-12-11 19:39:17 +0100

    Fusionant mètodes per eliminar redundància en codi

--- aula/apps/tutoria/avis_tutor_faltes.py
+++ aula/apps/tutoria/avis_tutor_faltes.py
@@ -1,105 +1,131 @@
-from aula.apps.alumnes.models import Alumne
 from aula.apps.alumnes.named_instances import curs_any_fi
 from datetime import timedelta, datetime
 from django.apps import apps
 from django.conf import settings
 from aula.apps.missatgeria.missatges_a_usuaris import ALUMNE_GENERAR_CARTA, tipusMissatge
-from aula.apps.missatgeria.models import Missatge
-from django.contrib.auth.models import User, Group
+from django.contrib.auth.models import User
 from django.urls import reverse
 
+def calcular_dades_carta(alumne, data_carta=None):
+    if data_carta is None:
+        data_carta = datetime.today().date()
 
-def avisTutorCartaPerFaltes():
-    totalcartes = 0
-    for alumne in Alumne.objects.filter(data_baixa__isnull=True):
-        tutors = alumne.tutorsDeLAlumne()
-        cartesDeLAlumne = alumne.cartaabsentisme_set.exclude(carta_esborrada_moment__isnull=False)
-        if cartesDeLAlumne:
-            darrera_carta = cartesDeLAlumne.order_by('-carta_numero')[0]
-            faltes_des_de_data = darrera_carta.faltes_fins_a_data + timedelta(days=1)
-        else:
-            faltes_des_de_data = alumne.grup.curs.data_inici_curs
-            darrera_carta = None
-        faltes_fins_a_data = datetime.today().date() - timedelta(days=3)
+    errors = []
 
-        # comprovo que hi ha més de 15 faltes:
-        EstatControlAssistencia = apps.get_model('presencia', 'EstatControlAssistencia')
-        falta = EstatControlAssistencia.objects.get(codi_estat='F')
-        nfaltes = (
-            alumne.controlassistencia_set.filter(impartir__dia_impartir__range=(faltes_des_de_data, faltes_fins_a_data))
-            .filter(estat=falta)
-            .count()
-        )
+    if data_carta and data_carta > datetime.today().date(): errors.append(u'Revisa la data de la carta.' )
+    EstatControlAssistencia = apps.get_model('presencia', 'EstatControlAssistencia')
+    falta = EstatControlAssistencia.objects.get(codi_estat='F')
 
-        if darrera_carta:
-            carta_numero = darrera_carta.carta_numero +1
-        else:
-            carta_numero = 1
+    # Determinar darrera carta i data d'inici de faltes
+    try:
+        darrera_carta = alumne.cartaabsentisme_set.exclude(carta_esborrada_moment__isnull=False).order_by('-carta_numero')[0]
+        carta_numero = darrera_carta.carta_numero + 1
+        faltes_des_de_data = darrera_carta.faltes_fins_a_data + timedelta(days=1)
+    except IndexError:
+        darrera_carta = None
+        carta_numero = 1
+        faltes_des_de_data = alumne.grup.curs.data_inici_curs
 
-        # Decideix el màxim de cartes i les faltes per carta segons el nivell i el número de la carta.
-        # Fa falta CUSTOM_FALTES_ABSENCIA_PER_NIVELL_NUM_CARTA
-        if len(settings.CUSTOM_FALTES_ABSENCIA_PER_NIVELL_NUM_CARTA) > 0:
+    # Determinar data de fi i nombre de faltes
+    try:
+        faltes_fins_a_data = (
+                                 alumne
+                                 .controlassistencia_set
+                                 .filter( impartir__dia_impartir__lte = data_carta )
+                                 .values_list( 'impartir__dia_impartir', flat=True )
+                                 .order_by( '-impartir__dia_impartir')
+                                 .distinct()[3]
+                                 )
+    except IndexError:
+        faltes_fins_a_data = data_carta - timedelta( days = 3 )
+
+
+    nfaltes = (
+        alumne.controlassistencia_set.filter(impartir__dia_impartir__range=(faltes_des_de_data, faltes_fins_a_data))
+        .filter(estat=falta)
+        .count()
+    )
+
+    # Calcular tipus de carta i llindar per generació de carta
+    tipus_carta = None
+    try:
+        te_mes_de_16 = (curs_any_fi() - alumne.data_neixement.year) > 16
+    except:
+        te_mes_de_16 = False
+    perNivell = True if len(settings.CUSTOM_FALTES_ABSENCIA_PER_NIVELL_NUM_CARTA) > 0 else False
+    if perNivell:
             faltes = settings.CUSTOM_FALTES_ABSENCIA_PER_NIVELL_NUM_CARTA.get(alumne.getNivellCustom())
-            if faltes is not None:
-                maxCartes = len(faltes)
+            maxCartes = len(faltes) if faltes else 0
+            index = carta_numero if data_carta is None else carta_numero - 1
+            llindar = faltes[index] if carta_numero <= maxCartes else 0
+            tipus_carta=''
+            if carta_numero>maxCartes and maxCartes>0:
+                errors.append(u'Aquest alumne ha arribat al màxim de cartes' )
             else:
-                maxCartes = 0
-            if carta_numero <= maxCartes:
-                llindar = faltes[carta_numero]
-            else:
-                llindar = 0
-            perNivell = True
-        else:
-            perNivell = False  # si False, no fa servir CUSTOM_FALTES_ABSENCIA_PER_NIVELL_NUM_CARTA
-
-
-        #càlcul del tipus de carta
-        tipus_carta=None
-        try:
-            te_mes_de_16 = (curs_any_fi() - alumne.data_neixement.year) > 16
-        except:
-            te_mes_de_16 = False
-
-        if perNivell:
-            tipus_carta = ''
-        else:
+                if llindar==0:
+                    errors.append(u"Error triant la carta a enviar a la família. Alumne {0}, sense nombre màxim de faltes per enviar carta".formnat(alumne))
+    else:
             if False:
                 pass
-            elif carta_numero in [1, 2, ] and alumne.cursa_nivell(u"ESO"):
-                tipus_carta = 'tipus{0}'.format(carta_numero)
-            elif carta_numero == 3 and alumne.cursa_nivell(u"ESO") and not te_mes_de_16:
-                tipus_carta = 'tipus3A'
-            elif carta_numero == 3 and alumne.cursa_nivell(u"ESO") and te_mes_de_16:
-                tipus_carta = 'tipus3C'
-            elif carta_numero in [1, 2, 3, ] and alumne.cursa_nivell(u"BTX"):
+            elif carta_numero in [1,2,] and alumne.cursa_nivell(u"ESO"):
+                tipus_carta = 'tipus{0}'.format( carta_numero  )
+            elif carta_numero == 3 and alumne.cursa_nivell("ESO"):
+                tipus_carta = 'tipus3A' if not te_mes_de_16 else 'tipus3C'
+            elif carta_numero in [1,2,3,] and alumne.cursa_nivell(u"BTX"):
                 tipus_carta = 'tipus3B'
-            elif carta_numero in [1, 2, 3, ] and alumne.cursa_nivell(u"CICLES"):
+            elif carta_numero in [1,2,3,] and alumne.cursa_nivell(u"CICLES"):
                 tipus_carta = 'tipus3D'
+            elif carta_numero in [1,2,3,]:
+                errors.append(u"Error triant la carta a enviar a la família. Alumne {0}, sense nivell assignat".format(alumne))
             else:
-                tipus_carta = ''
-
+                errors.append(u'Aquest alumne ha arribat al màxim de cartes' )
             llindar = settings.CUSTOM_FALTES_ABSENCIA_PER_TIPUS_CARTA.get(tipus_carta,
                                                                           settings.CUSTOM_FALTES_ABSENCIA_PER_CARTA)
 
-        if nfaltes >= llindar:
-            missatge = ALUMNE_GENERAR_CARTA
-            txt = missatge.format(alumne)
-            enllac = reverse('tutoria__cartes_assistencia__gestio_cartes')
-            tipus_de_missatge = tipusMissatge(missatge)
-            Missatge = apps.get_model('missatgeria', 'Missatge')
-            usuari_notificacions, new = User.objects.get_or_create(username='TP')
-            if new:
-                usuari_notificacions.is_active = False
-                usuari_notificacions.first_name = u"Usuari Tasques Programades"
-                usuari_notificacions.save()
+    return {
+        'carta_numero': carta_numero,
+        'tipus_carta': tipus_carta,
+        'faltes_des_de_data': faltes_des_de_data,
+        'faltes_fins_a_data': faltes_fins_a_data,
+        'nfaltes': nfaltes,
+        'llindar': llindar,
+        'errors': errors
+    }
 
-            msg = Missatge(remitent=usuari_notificacions, text_missatge=txt, enllac=enllac,
-                           tipus_de_missatge=tipus_de_missatge)
-            for tutor in tutors:
-                importancia = 'VI'
-                msg.envia_a_usuari(tutor, importancia)
-                print("Enviat missatge a tutor/a {0}, de l'alumne {1}".format(tutor, alumne))
+
+def enviar_missatge_tutor(alumne):
+    #Envia un missatge als tutors d'un alumne sobre una carta d'absentisme.
+    tutors = alumne.tutorsDeLAlumne()
+    missatge = ALUMNE_GENERAR_CARTA
+    txt = missatge.format(alumne)
+    enllac = reverse('tutoria__cartes_assistencia__gestio_cartes')
+    tipus_de_missatge = tipusMissatge(missatge)
+
+    Missatge = apps.get_model('missatgeria', 'Missatge')
+    usuari_notificacions, new = User.objects.get_or_create(username='TP')
+    if new:
+        usuari_notificacions.is_active = False
+        usuari_notificacions.first_name = "Usuari Tasques Programades"
+        usuari_notificacions.save()
+
+    msg = Missatge(
+        remitent=usuari_notificacions,
+        text_missatge=txt,
+        enllac=enllac,
+        tipus_de_missatge=tipus_de_missatge,
+    )
+
+    for tutor in tutors:
+        importancia = 'VI'  # Nivel de importancia
+        msg.envia_a_usuari(tutor, importancia)
+        print("Enviat missatge al tutor/a {0}, de l'alumne/a {1}".format(tutor,alumne))
+
+def avisTutorCartaPerFaltes():
+    from aula.apps.alumnes.models import Alumne
+    totalcartes = 0
+    for alumne in Alumne.objects.filter(data_baixa__isnull=True):
+        dades_carta = calcular_dades_carta(alumne)
+        if dades_carta['nfaltes'] >= dades_carta['llindar']:
+            enviar_missatge_tutor(alumne)
             totalcartes += 1
-
-    print("Total missatges enviats: {0}".format(totalcartes))
-
+    print(f"Total missatges enviats: {totalcartes}")
--- aula/apps/tutoria/business_rules/cartaaabsentisme.py
+++ aula/apps/tutoria/business_rules/cartaaabsentisme.py
@@ -1,16 +1,10 @@
 # This Python file uses the following encoding: utf-8
 from django.core.exceptions import ValidationError
-from datetime import timedelta, date
-from django.apps import apps
+from aula.apps.tutoria.avis_tutor_faltes import calcular_dades_carta
 
-
-
-from aula.apps.alumnes.named_instances import curs_any_fi
-from django.conf import settings
 
 def cartaabsentisme_clean( instance ):
     '''
-    
     calcular tipus:
         tipus1 = primera carta tots
         tipus2 = segona carta tots
@@ -18,143 +12,28 @@ def cartaabsentisme_clean( instance ):
         tipus3B = carta Batxillerat
         tipus3C = cursa obligatoria i té més de 16 anys
         tipus3D = carta Cicles
-
     '''
     errors = []
+    llindar = 0
     instance.__is_update = instance.pk is not None
     if not instance.__is_update:
-        if instance.data_carta is None:
-            instance.data_carta = date.today()
-
-        if instance.data_carta > date.today():
-            errors.append(u'Revisa la data de la carta.' )
-
-        #busco darrera carta:
-        try:
-            darrera_carta = instance.alumne.cartaabsentisme_set.exclude( carta_esborrada_moment__isnull = False ).order_by( '-carta_numero' )[0]
-            darrera_carta_n = darrera_carta.carta_numero
-        except IndexError:
-            darrera_carta = None
-            darrera_carta_n = 0
-        carta_numero = darrera_carta_n + 1
-
-        #data des d'on començar a comptar
-        if bool( darrera_carta ):
-            faltes_des_de_data = darrera_carta.faltes_fins_a_data + timedelta( days = 1 )
-
-        else:
-         try:
-                faltes_des_de_data =(instance
-                                     .alumne
-                                     .controlassistencia_set
-                                     .values_list( 'impartir__dia_impartir', flat=True )
-                                     .order_by( 'impartir__dia_impartir')[0]
-                                     )
-         except IndexError:
-                faltes_des_de_data =(instance
-                                     .alumne
-                                     .grup
-                                     .curs
-                                     .data_inici_curs
-                                     )
-
-        #data fins on comptar:
-        try:
-            faltes_fins_a_data = (
-                                 instance
-                                 .alumne
-                                 .controlassistencia_set
-                                 .filter( impartir__dia_impartir__lte = instance.data_carta )
-                                 .values_list( 'impartir__dia_impartir', flat=True )
-                                 .order_by( '-impartir__dia_impartir')
-                                 .distinct()[4]
-                                 )
-        except IndexError:
-            faltes_fins_a_data = instance.data_carta - timedelta( days = 3 )
-        
-        #comprovo que hi ha més de 15 faltes:
-        EstatControlAssistencia = apps.get_model('presencia', 'EstatControlAssistencia')
-        falta = EstatControlAssistencia.objects.get( codi_estat = 'F'  )
-        nfaltes = (
-                   instance
-                   .alumne
-                   .controlassistencia_set
-                   .filter( impartir__dia_impartir__range = ( faltes_des_de_data, faltes_fins_a_data )   )
-                   .filter( estat = falta )
-                   .count()
-                   )
-        # amorilla@xtec.cat
-        # Decideix el màxim de cartes i les faltes per carta segons el nivell i el número de la carta.
-        # Fa falta CUSTOM_FALTES_ABSENCIA_PER_NIVELL_NUM_CARTA
-        # No fa falta definir el tipus de carta en aquest moment, es farà al document odt
-        if len(settings.CUSTOM_FALTES_ABSENCIA_PER_NIVELL_NUM_CARTA)>0:
-            faltes = settings.CUSTOM_FALTES_ABSENCIA_PER_NIVELL_NUM_CARTA.get(instance.alumne.getNivellCustom())
-            if faltes is not None:
-                maxCartes = len(faltes)
-            else:
-                maxCartes = 0
-            if carta_numero<=maxCartes:
-                llindar = faltes[carta_numero-1]
-            else:
-                llindar=0
-            perNivell=True
-        else:
-            perNivell=False  # si False, no fa servir CUSTOM_FALTES_ABSENCIA_PER_NIVELL_NUM_CARTA 
-
-        #calculo tipus de carta    
-        tipus_carta=None
-        try:
-            te_mes_de_16 = (curs_any_fi() - instance.alumne.data_neixement.year) > 16
-        except:
-            te_mes_de_16 = False
-
-        # amorilla@xtec.cat
-        if perNivell:
-            tipus_carta=''
-            if carta_numero>maxCartes and maxCartes>0:
-                errors.append(u'Aquest alumne ha arribat al màxim de cartes' )
-            else:
-                if llindar==0:
-                    raise Exception(u"Error triant la carta a enviar a la família")
-        else:
-            
-            if False:
-                pass
-
-            elif carta_numero in [1,2,] and instance.alumne.cursa_nivell(u"ESO"):
-                tipus_carta = 'tipus{0}'.format( carta_numero  )
-
-            elif carta_numero == 3 and instance.alumne.cursa_nivell(u"ESO") and not te_mes_de_16:
-                tipus_carta = 'tipus3A'
-
-            elif carta_numero == 3 and instance.alumne.cursa_nivell(u"ESO") and te_mes_de_16:
-                tipus_carta = 'tipus3C'
-
-            elif carta_numero in [1,2,3,] and instance.alumne.cursa_nivell(u"BTX"):
-                tipus_carta = 'tipus3B'
-
-            elif carta_numero in [1,2,3,] and instance.alumne.cursa_nivell(u"CICLES"):
-                tipus_carta = 'tipus3D'
-
-            elif carta_numero in [1,2,3,]:
-                raise Exception(u"Error triant la carta a enviar a la família")
-
-            else:
-                errors.append(u'Aquest alumne ha arribat al màxim de cartes' )
-
-            llindar = settings.CUSTOM_FALTES_ABSENCIA_PER_TIPUS_CARTA.get(instance.tipus_carta,settings.CUSTOM_FALTES_ABSENCIA_PER_CARTA)
-
-        instance.carta_numero = carta_numero
-        instance.tipus_carta = tipus_carta
-        instance.faltes_des_de_data = faltes_des_de_data
-        instance.faltes_fins_a_data = faltes_fins_a_data
-        instance.nfaltes = nfaltes
+        dades_carta = calcular_dades_carta(instance.alumne, instance.data_carta)
+        if dades_carta['nfaltes'] < dades_carta['llindar']:
+            raise ValidationError(
+                u'Aquest alumne no ha acumulat {} faltes des de la darrera carta'.format(dades_carta['llindar']))
+        instance.carta_numero = dades_carta['carta_numero']
+        instance.tipus_carta = dades_carta['tipus_carta']
+        instance.faltes_des_de_data = dades_carta['faltes_des_de_data']
+        instance.faltes_fins_a_data = dades_carta['faltes_fins_a_data']
+        instance.nfaltes = dades_carta['nfaltes']
+        errors = dades_carta['errors']
+        llindar = dades_carta['llindar']
 
     if instance.nfaltes < llindar:
         errors.append(u'Aquest alumne no ha acumulat {} faltes des de la darrera carta'.format(llindar))
 
     if len( errors ) > 0:
-        raise ValidationError(errors)    
+        raise ValidationError(errors)
     
                                          
 
